027dec02daad2d7b055873c61f1adcd8043236cb
MAYUR <74107137+mayur1377@users.noreply.github.com>
2022-07-29 19:44:15 +0530

    Time: 7 ms (55.39%), Space: 8.7 MB (29.81%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -3,37 +3,21 @@ public:
 vector<string>ans;
 void help(string a , string b)
 {
-    unordered_map<char , char>m , n;
+    unordered_map<char , char>m ;
+    vector<int>seen(26 , 0);
+    
     if(a.length()!=b.length()) return ;
     int flag=1;
     for(int i=0 ; i<b.length() ; i++)
     {
-        if(m.find(b[i])==m.end())
+        if(m.find(b[i])==m.end() and seen[a[i]-'a']==0)
         {
-            m[b[i]]=a[i];
+            m[b[i]]=a[i]  , seen[a[i]-'a']=1;
         }
         else if(m[b[i]]!=a[i]) flag=0;
     }
-    for(int i=0 ; i<b.length() ; i++)
-    {
-        if(n.find(a[i])==n.end())
-        {
-            n[a[i]]=b[i];
-        }
-        else if(n[a[i]]!=b[i]) flag=0;
-    }
-    string x="";
-    for(int i=0 ; i<a.length() ; i++)
-    {
-        x+=m[b[i]];
-    }
-    string y="";
-    for(int i=0 ; i<a.length() ; i++)
-    {
-        y+=n[a[i]];
-    }
-    if(x==a and y==b) ans.push_back(a);
-    cout<<x<<" "<<y<<" \n";
+    if(flag) ans.push_back(a);
+ 
     
 }
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
