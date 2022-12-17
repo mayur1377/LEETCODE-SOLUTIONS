75b84d70e6a8a0a7252e3acffdfe68b7a9867085
MAYUR <74107137+mayur1377@users.noreply.github.com>
2022-12-17 17:13:17 +0530

    Time: 55 ms (73.89%), Space: 20.3 MB (13.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& nums) {
-vector<vector<int>>ans;
-sort(nums.begin() , nums.end());
-ans.push_back(nums[0]);
-for(int i=1  ;i<nums.size(); i++)
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+sort(intervals.begin() , intervals.end());
+stack<pair<int , int>>s;
+s.push({intervals[0][0] ,intervals[0][1]});
+for(int i=1 ; i<intervals.size() ; i++)
 {
-    if(ans.back()[1]>=nums[i][0])
+    if(s.top().second>=intervals[i][0])
     {
-        ans.back()[1]=max(ans.back()[1] , nums[i][1]);
+        s.top().second=max(s.top().second , intervals[i][1]);
     }
-    else ans.push_back(nums[i]);
+    else s.push({intervals[i][0] , intervals[i][1]});
+}
+vector<vector<int>>ans;
+while(s.size())
+{
+    vector<int>temp;
+    int x=s.top().first;
+    temp.push_back(x);
+    x=s.top().second;
+    temp.push_back(x);
+    ans.push_back(temp);
+    s.pop();
 }
 return ans;
     }
