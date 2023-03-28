5000780da86f327265c493105aee57a1f84a3240
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-03-28 21:53:04 +0530

    Time: 8 ms (30.37%), Space: 9.6 MB (81.02%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-int help(vector<int>days , vector<int>costs , int i , vector<int>&dp)
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+vector<int>dp(366 , 0);
+for(int i=days.size()-1 ; i>=0 ; i--)
 {
-    if(i>=days.size()) return 0;
-    
     int a=INT_MAX  ,b=INT_MAX , c=INT_MAX;
-    if(dp[i]!=-1) return dp[i];
-    a=costs[0]+help(days , costs , i+1 , dp);
+    a=costs[0]+dp[i+1];
     int temp=i;
     int oneweek=days[temp]+6;
     while(temp<days.size() and days[temp]<=oneweek) temp++;
-    b=costs[1]+help(days , costs , temp , dp);
-    
+    b=costs[1]+dp[temp];
     temp=i;
     int onemonth=days[temp]+29;
     while(temp<days.size() and days[temp]<=onemonth) temp++;
-    c=costs[2]+help(days , costs , temp , dp);
-    return dp[i]=min(a , min(b , c));    
-}
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-vector<int>dp(366 , -1);
-return help(days , costs , 0 , dp);
+    c=costs[2]+dp[temp];
+    dp[i]=min(a , min(b , c));    
+}      
+return dp[0];
     }
 };
