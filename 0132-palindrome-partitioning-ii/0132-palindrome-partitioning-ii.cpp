d1dcb438a16fea22239afb1d38334b39b1d681da
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-04-11 11:03:25 +0530

    Time: 419 ms (41.54%), Space: 20.3 MB (28.80%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
-    bool isPal(string &s) {
-        int i = 0, j = s.size() - 1;
-        while(i < j) {
+bool ispalin(string &s)
+{
+    int i = 0, j = s.size() - 1;
+    while(i < j) {
             if(s[i++] != s[j--]) return false;
-        }
-        return true;
     }
-    int dfs(int i, int n, string &s, vector<int> &dp) {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        string t;
-        int ans = 1e9;
-        for(int j=i; j<n; j++) {
-            t += s[j];
-            if(isPal(t)) ans = min(ans, 1 + dfs(j + 1, n, s, dp));
-        }
-        return dp[i] = ans;
+    return true;
+}
+int help(string &s , int n , int i , vector<int>&dp)
+{
+    if(i==n)  return 0;
+    if(dp[i]!=-1) return dp[i];
+    
+    string temp;
+    int ans=1e9;
+    for(int x=i ; x<n ; x++)
+    {
+        temp+=s[x];
+        if(ispalin(temp)) ans=min(ans , 1+help(s , n , x+1 , dp));
     }
+    return dp[i]=ans;
+}
 public:
     int minCut(string s) {
-        int n = s.size();
-        vector<int> dp(n, -1);
-        return dfs(0, n, s, dp) - 1;
+int n=s.size();
+vector<int>dp(n , -1);
+return help(s , n , 0 , dp)-1;
     }
 };
