cfd9b3f80865b30821b6e38eb41238f96ad96376
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-04-12 18:42:22 +0530

    Time: 2427 ms (6.47%), Space: 24.2 MB (38.06%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int dp[2001][2001];
-    bool isPal(string &s,int i,int j){
-        while(i<j){
-            if(s[i++]!=s[j--])return false;
-        }
-        return true;
+int dp[2001][2001];
+bool ispal(string &s , int i , int j)
+{
+    while(i<j){
+        if(s[i++]!=s[j--])return false;
     }
-    int solve(int i,int j,string &s,int k){
-        if(j>=s.size() || i>=s.size())return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(isPal(s,i,j)){
-            return dp[i][j]=max(solve(i,j+1,s,k),max(solve(j+1,j+k,s,k)+1,solve(i+1,j+1,s,k)));
-        }
-        return dp[i][j]=max(solve(i+1,j+1,s,k),solve(i,j+1,s,k));
+    return true;
+}
+    
+int solve(int i , int j , string &s , int k)
+{
+    if(i>=s.size() or j>=s.size()) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(ispal(s , i , j)){
+        return dp[i][j]=max(solve(i , j+1 , s , k ) , max(solve(i+1 , j+1 , s , k ) , solve(j+1 , j+k ,  s , k  )+1));
     }
+    return dp[i][j]=max(solve(i+1 ,j+1 , s , k ) , solve(i , j+1 , s , k  ));
+}
     int maxPalindromes(string s, int k) {
-        int n=s.size();
-        if(k==1)return n;
-        memset(dp,-1,sizeof(dp));
-        return solve(0,k-1,s,k);
+memset(dp,-1,sizeof(dp));
+if(k==1) return s.size();
+return solve(0 , k-1 , s , k );
     }
 };
