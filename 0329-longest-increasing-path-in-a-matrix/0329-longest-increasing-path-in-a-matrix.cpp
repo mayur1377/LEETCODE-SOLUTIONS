3b7b4ddb0823769bf0ee7ac4d3a3ad64e7600525
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-05-01 21:09:57 +0530

    Time: 55 ms (42.53%), Space: 15.9 MB (85.18%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-int dp[201][201];
-int help(vector<vector<int>>&matrix , int i , int j , int prev)
+int  dfs(vector<vector<int>>&matrix , vector<vector<int>>&dp , int i , int j , int prev)
 {
     if(i<0 or i>=matrix.size() or j<0 or j>=matrix[0].size()) return 0;
-    if(prev>=matrix[i][j]) return 0;
-    if(dp[i][j]) return dp[i][j];
-    int a=help(matrix , i+1 , j , matrix[i][j]);
-    int b=help(matrix , i-1 , j , matrix[i][j]);
-    int c=help(matrix , i , j+1 , matrix[i][j]);
-    int d=help(matrix , i , j-1 , matrix[i][j]);
-    return dp[i][j]=max({a, b , c , d})+1;
-
+    if(matrix[i][j]<=prev) return 0;
+    if(dp[i][j]!=0) return dp[i][j];
+    int count=1;
+    int a=dfs(matrix , dp , i+1 , j , matrix[i][j]);
+    int b=dfs(matrix , dp , i-1 , j , matrix[i][j]);
+    int c=dfs(matrix , dp , i , j+1 , matrix[i][j]);
+    int d=dfs(matrix , dp , i , j-1 , matrix[i][j]);
+    count=max(count , 1+max({a , b , c , d}));
+    return dp[i][j]=count;
 }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-int ans=INT_MIN;
+vector<vector<int>>dp(matrix.size() , vector<int>(matrix[0].size() , 0));
+int ans=1;
 for(int i=0 ; i<matrix.size() ; i++)
 {
     for(int j=0 ; j<matrix[0].size() ; j++)
     {
-        ans=max(ans, help(matrix , i , j , -1));
+        if(dp[i][j]==0)
+        {
+            ans=max(ans , dfs(matrix , dp , i , j , -1));
+        }
     }
 }
         return ans;
