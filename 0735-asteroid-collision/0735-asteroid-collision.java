656cbd5c12158666e2004249c922d39d9243ba09
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-05-24 06:48:01 +0530

    Time: 14 ms (18.79%), Space: 44.9 MB (5.91%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int ast : asteroids) {
+            if (ast > 0) {
+                stack.push(ast);
+            }
+            else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(ast)) {
+                    stack.pop();
+                }
+
+                if (stack.isEmpty() || stack.peek() < 0)  {
+                    stack.push(ast);
+                }
+
+                if (stack.peek() == Math.abs(ast)) {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.stream().mapToInt(i -> i).toArray();
+    }
+}
