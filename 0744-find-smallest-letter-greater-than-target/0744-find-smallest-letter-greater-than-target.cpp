edbf5b25a3983509f0028a371fcd20172c92ca79
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-06-09 21:37:00 +0530

    Time: 21 ms (28.25%), Space: 15.7 MB (96.78%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+int low=0;
+int high=letters.size()-1;
+while(low<=high)
+{
+    int mid=low+(high-low)/2;
+    if(letters[mid]>target) high=mid-1;
+    else low=mid+1;
+}
+        return letters[low%letters.size()];
+    }
+};
