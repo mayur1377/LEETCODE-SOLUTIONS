9093bef2a852760ac985f68685a46f3d63b19f2d
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-08-01 02:09:41 +0530

    Time: 7 ms (26.72%), Space: 11.6 MB (5.22%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
+void help(vector<int>nums,vector<vector<int>>&ans ,vector<int>temp , unordered_map<int , int>m)
+{
+    if(temp.size()==nums.size())
+    {
+        ans.push_back(temp);
+        return ;
+    }
+    for(int i=0 ; i<nums.size() ; i++)
+    {
+        if(m[nums[i]]==0)
+        {
+            m[nums[i]]=1;
+            temp.push_back(nums[i]);
+            help(nums , ans , temp , m);
+            temp.pop_back();
+            m[nums[i]]=0;
+        }
+    }
+}
     vector<vector<int>> permute(vector<int>& nums) {
 vector<vector<int>>ans;
-vector<int>temp=nums;
-ans.push_back(nums);
-next_permutation(temp.begin() , temp.end());
-while(nums!=temp) 
-{
-    ans.push_back(temp);
-    next_permutation(temp.begin() , temp.end());
-}
-sort(ans.begin(),ans.end());
+unordered_map<int ,int>m;
+for(auto i : nums)m[i]=0;
+vector<int>temp;
+help(nums, ans , temp , m);
 return ans;
     }
 };
