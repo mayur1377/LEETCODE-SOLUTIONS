4b97bd1902bf6ecaf20e40dbba7d7f19b25603f2
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-08-02 20:49:44 +0530

    Time: 3 ms (78.27%), Space: 11 MB (44.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,18 +6,17 @@ int high=nums.size()-1;
 while(low<=high)
 {
     int mid=(low+high)>>1;
-    cout<<"in mid "<<mid<<endl;
     if(nums[mid]==target) return mid;
     if(nums[low]<=nums[mid])
     {
-        //left part is sorted
+//         left part is sorted
         if(nums[low]<=target and target<=nums[mid])
         {
             high=mid-1;
         }
         else low=mid+1;
     }
-    else 
+    else
     {
         if(nums[mid]<=target and target<=nums[high])
         {
@@ -26,6 +25,6 @@ while(low<=high)
         else high=mid-1;
     }
 }
-return -1;
+        return -1;
     }
 };
