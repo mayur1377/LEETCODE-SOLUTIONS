7bb87b59da544ee35265ffda8342e3257ae77eb4
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-08-02 18:32:40 +0530

    Time: 897 ms (32.87%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N=N-1;
+  RETURN (
+      SELECT DISTINCT(salary) 
+      from Employee 
+      order by salary DESC
+      LIMIT 1 OFFSET N
+  );
+END
