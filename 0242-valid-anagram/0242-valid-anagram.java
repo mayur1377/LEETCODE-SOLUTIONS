a124a08a94bdd945e0a027bec9c20f56f73940c3
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-08-15 12:07:24 -0700

    Time: 21 ms (15.87%), Space: 41.8 MB (92.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> smap=new HashMap<>();
+        int sl=s.length();
+        int tl=t.length();
+        if(sl!=tl){return false;}
+        for(int i=0;i<sl;i++){
+            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+            smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
+        }
+        for(char c:smap.keySet()){
+            if(smap.get(c)!=0){return false;}
+        }
+        
+        return true;
+    }
+}
