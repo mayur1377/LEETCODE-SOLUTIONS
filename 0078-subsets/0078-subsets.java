bb0332ae0512311e046ba156790e938f66b78756
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-08-16 06:19:06 -0700

    Time: 0 ms (100.00%), Space: 41.4 MB (98.46%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        helper(ans, 0, nums, list);
+        return ans;
+    }
+
+    public void helper(
+        List<List<Integer>> ans,
+        int start,
+        int[] nums,
+        List<Integer> list
+    ) {
+        if (start >= nums.length) {
+            ans.add(new ArrayList<>(list));
+        } else {
+            // add the element and start the  recursive call
+            list.add(nums[start]);
+            helper(ans, start + 1, nums, list);
+            // remove the element and do the backtracking call.
+            list.remove(list.size() - 1);
+            helper(ans, start + 1, nums, list);
+        }
+    }
+};
